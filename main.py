36bc38ccae93525fa9c47d32885393fc79c51b76
andrejvasiliu <106980546+andrejvasiliu@users.noreply.github.com>
2025-07-19 16:08:40 +0100

    setting up pygame

--- main.py
+++ main.py
@@ -7,6 +7,21 @@ def main():
 f"""Screen width: {SCREEN_WIDTH}
 Screen height: {SCREEN_HEIGHT}""")
 
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0, 0, 0))  # Fill the screen with black
+        pygame.display.flip()  # Update the display
+        dt = clock.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
